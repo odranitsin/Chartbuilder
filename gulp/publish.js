1608282b4d0fd6875312fe2c030f3dba1d75fff7
Brad Oyler <bradoyler@gmail.com>
2016-07-07 21:35:41 -0400

    adds aws-publish task for deploying to S3

---
+++ gulp/publish.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+	rename = require('gulp-rename'),
+	awspublish = require('gulp-awspublish');
+
+const debugMode = false;
+
+gulp.task('publish', function() {
+
+	var aws;
+	try {
+		aws = require('../config.json').AWS;
+	}
+	catch (ex) {
+		console.log('>>> no config.json found', ex);
+	}
+
+	var publisher = awspublish.create({
+		region: aws.Region,
+		params: {Bucket: aws.Bucket},
+		accessKeyId: aws.Credentials.AccessKeyId,
+		secretAccessKey: aws.Credentials.SecretAccessKey
+	});
+
+	return gulp.src(['build/**/*.{html,css,js}'])
+		.pipe(rename(function (path) {
+			if(aws.FolderPath) {
+				path.dirname = '/'+ aws.FolderPath +'/'+ path.dirname;
+			}
+		}))
+		.pipe(awspublish.gzip())
+		.pipe(publisher.publish({}, {simulate: debugMode, createOnly: false}))
+		.pipe(publisher.cache())
+		.pipe(awspublish.reporter());
+});
