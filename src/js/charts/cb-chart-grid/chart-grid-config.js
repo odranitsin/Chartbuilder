ae58d63ec6d331a0a76ffb0cd936abc3c263ef9a
dvrnd <devrand@protonmail.ch>
2016-09-08 09:35:20 +0300

    more or less all padding issues are solved

--- src/js/charts/cb-chart-grid/chart-grid-config.js
+++ src/js/charts/cb-chart-grid/chart-grid-config.js
@@ -24,18 +24,18 @@ var ChartConfig = require("../ChartConfig");
 * @property {object} padding - Distances btwn inner chart elements and container
 */
 var display = {
-	afterTitle: "3em", // distance between top of title and top of legend or chart
-	afterLegend: "-2em", // distance between top of legend and top of chart
+	afterTitle: "2em", // distance between top of title and top of legend or chart
+	afterLegend: 0, // distance between top of legend and top of chart
 	blockerRectOffset: 3, // distance between text and background blocker rect
 	paddingPerBar: "0.6em", // extra space around bars
 	barHeight: "0.6em", // height of each bars
-	afterXYBottom: "2em",
+	afterXYBottom: "1em",
 	afterXYRight: "1.9em",
-	columnExtraPadding: "0.1em",
+	columnExtraPadding: 0,
 	bottomPaddingWithoutFooter: "0.5em",
 	xy: require("../cb-xy/xy-config").display,
 	margin: {
-		top: "1.9em",
+		top: "2.2em",
 		right: "2em",
 		bottom: "0.5em",
 		left: "1em"
--- src/js/config/chart-sizes.js
+++ src/js/config/chart-sizes.js
@@ -7,7 +7,7 @@
 var chart_sizes = {
 	medium: {
 		width: 700,
-		height: 550
+		height: 525
 	},
 	spotLong: {
 		width: 320,
